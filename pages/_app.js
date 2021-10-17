b65d10a5b150950a34b6c256b6139dc54d547342
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-10-17 18:13:38 -0400

    switched head data to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Navigation from "../components/Navigation";
 
 import "../styles/globals.css";
@@ -5,6 +7,11 @@ import "../styles/globals.css";
 function Kopelen({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Kopelen</title>
+        <meta name="description" content="Kopelen does things" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navigation />
       <div id="main">
         <Component {...pageProps} />

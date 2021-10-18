15de7232e691c14bb0dcd7200e4effca50812f5b
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-10-18 15:51:45 -0400

    add comment buttons and styling

--- components/Threads/FullThreads/CommentSection/Comment/index.js
+++ components/Threads/FullThreads/CommentSection/Comment/index.js
@@ -1,12 +1,24 @@
-//
+import {
+  MdDeleteForever,
+  MdEmojiEmotions,
+  MdOutlinedFlag,
+} from "react-icons/md";
 
 import classes from "./Comment.module.css";
 
 const Comment = (props) => {
+  const isModerator = false;
+
   return (
     <div className={classes.comment}>
       <div className={classes.author}>{props.author}</div>
+      <div className={classes.action}>
+        {isModerator ? <MdDeleteForever /> : <MdOutlinedFlag />}
+      </div>
       <div className={classes.content}>{props.content}</div>
+      <div className={classes.interact}>
+        <MdEmojiEmotions className={classes.blueHover} />
+      </div>
     </div>
   );
 };

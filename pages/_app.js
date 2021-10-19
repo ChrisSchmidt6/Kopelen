9f02e425421f41afe48c4430347b2fd7fee977e8
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-10-18 20:07:20 -0400

    change global font

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,12 @@ function Kopelen({ Component, pageProps }) {
         <title>Kopelen</title>
         <meta name="description" content="Kopelen does things" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Navigation />
       <div id="main">

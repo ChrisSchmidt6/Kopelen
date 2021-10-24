e462f8d7f3959ae7f21d3f341a43d28984f9b974
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-10-23 21:58:59 -0400

    add error elements and styling

--- components/ui/Input/index.js
+++ components/ui/Input/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { useField } from "formik";
 
 import classes from "./Input.module.css";
 
@@ -6,32 +7,53 @@ const Input = (props) => {
   const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef(null);
 
-  const handleFocus = () => {
+  const [field, meta] = useField(props);
+
+  const handleFocus = (e) => {
+    if (inputRef.current.value.length > 0) setIsFocused(true);
+  };
+
+  const focusField = () => {
     setIsFocused(true);
     inputRef.current.focus();
   };
 
-  const handleBlur = () => {
+  const handleBlur = (e) => {
     if (inputRef.current.value.length === 0) setIsFocused(false);
+    field.onBlur(e);
   };
 
   useEffect(() => {
     if (inputRef.current.value.length > 0) setIsFocused(true);
   }, []);
 
+  const showError = meta.touched && meta.error;
+  const errorClass = showError ? classes.error : null;
+
+  const labelClasses = `${isFocused ? classes.active + " " : ""}${errorClass}`;
+
   return (
     <>
-      <div className={classes.inputGroup} onClick={handleFocus}>
-        <label className={isFocused ? classes.active : ""} htmlFor={props.name}>
-          {props.label}
-        </label>
-        <input
-          name={props.name}
-          type={props.type}
-          ref={inputRef}
-          onBlur={handleBlur}
-          onFocus={handleFocus}
-        ></input>
+      <div className={classes.container}>
+        <div className={classes.inputGroup} onClick={focusField}>
+          <label className={labelClasses} htmlFor={props.name}>
+            {props.label}
+          </label>
+          <input
+            className={errorClass}
+            name={props.name}
+            type={props.type}
+            ref={inputRef}
+            {...field}
+            onBlur={(e) => {
+              handleBlur(e);
+            }}
+            onFocus={(e) => {
+              handleFocus(e);
+            }}
+          />
+        </div>
+        {showError ? <small>{meta.error}</small> : null}
       </div>
     </>
   );

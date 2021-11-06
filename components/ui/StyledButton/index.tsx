8073a436bb92269467f73cad11cdec5d8f3fb0c7
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-11-05 22:44:07 -0400

    create StyledButton UI component

---
+++ components/ui/StyledButton/index.tsx
@@ -0,0 +1,11 @@
+import classes from './StyledButton.module.css';
+
+const StyledButton: React.FC<{ children: string, handleClick: () => void }> = (props) => {
+    return (
+        <button className={classes.styledButton} onClick={props.handleClick} tabIndex={-1}>
+          {props.children}
+        </button>
+    );
+}
+
+export default StyledButton;

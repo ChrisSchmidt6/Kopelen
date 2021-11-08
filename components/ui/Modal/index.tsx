5d31c7b419acbdcc7bccae3f694b7938eeb74c6c
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-11-07 20:30:20 -0500

    create Modal UI component"

---
+++ components/ui/Modal/index.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from "react";
+import { createPortal } from "react-dom";
+import { MdClose } from "react-icons/md";
+
+import IconButton from "../IconButton";
+
+import classes from "./Modal.module.css";
+
+const Modal: React.FC<{
+  handleClose: () => void;
+  header: ReactElement;
+  main: ReactElement;
+  footer: ReactElement;
+}> = (props) => {
+
+  return createPortal(
+    <>
+      <div className="container" onClick={props.handleClose}></div>
+      <div className={classes.modal}>
+        <div className={classes.modalBody}>
+          <header>{props.header}</header>
+          <main>{props.main}</main>
+          <footer>
+            <IconButton onClick={props.handleClose}>
+              <MdClose />
+            </IconButton>
+          </footer>
+        </div>
+      </div>
+    </>,
+    document.getElementById("modal")! 
+  );
+};
+
+export default Modal;

88e7eb897fc2c08134db367c1c29f7fe3a6c9b7b
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-11-10 22:56:03 -0500

    get thread data from threadsSlice state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { MdPostAdd } from "react-icons/md";
+import { useAppSelector } from "hooks/reduxHooks";
 
 import ThreadPreviews from "components/Threads/ThreadPreviews";
 import ToggleButton from "components/UI/ToggleButton";
@@ -7,35 +8,11 @@ import StyledButton from "components/UI/StyledButton";
 
 import classes from "./Home.module.css";
 
-const DUMMY_DATA = [
-  {
-    id: "t1",
-    type: "text",
-    author: "chris",
-    title: "Text Thread Preview Example",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vestibulum ligula et nunc tincidunt, a egestas lorem laoreet. Vestibulum justo ex, ultricies vel luctus eu, vulputate id lorem. Vivamus posuere venenatis dolor quis condimentum. Fusce et mi suscipit, tincidunt urna ac, egestas velit. Praesent accumsan sollicitudin facilisis. Morbi pellentesque arcu id massa viverra molestie eget sit amet velit. Vestibulum suscipit arcu justo, ut mattis arcu aliquam in. Morbi est arcu, convallis at tempus id, pharetra eu purus.",
-  },
-  {
-    id: "t9",
-    type: "image",
-    author: "cupe",
-    title: "Image Thread Preview Example",
-    data: "https://www.gannett-cdn.com/presto/2020/04/16/USAT/5b7ef814-a04d-44c8-86ef-1d47c798a1f1-Golds_gym_CharlesTown.jpg",
-  },
-  {
-    id: "t10",
-    type: "video",
-    author: "potter",
-    title: "Video Thread Preview Example",
-    data: "https://www.youtube.com/watch?v=XU9Njr5ci3c",
-  },
-];
-
 const Home = () => {
   const router = useRouter();
   const query = router.query;
 
-  let threadData = [...DUMMY_DATA];
+  let threadData = useAppSelector(state => state.threadsSlice.threads);
   if (query["sort"] === "desc") threadData = threadData.reverse();
 
   const ThreadsBundle = threadData.map((thread) => {

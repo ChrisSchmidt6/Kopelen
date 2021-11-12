e33072d952805e08f13d9d16c930b1ddb29d3e81
Chris Schmidt <cmatthewschmidt@gmail.com>
2021-11-12 00:03:27 -0500

    move ThreadTitle styling to local folder

--- components/Threads/ThreadContainer/ThreadTitle/index.tsx
+++ components/Threads/ThreadContainer/ThreadTitle/index.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
+import classes from "./ThreadTitle.module.css";
+
 const ThreadTitle: React.FC<{
   id: string;
   title: string;
   view: "full" | "preview";
 }> = (props) => {
   if (props.view === "full") {
-    return <h2>{props.title}</h2>;
+    return <h2 className={classes.titleFull}>{props.title}</h2>;
   } else {
     return (
       <Link href={`/thread/${props.id}`} passHref>
-        <h4>{props.title}</h4>
+        <h4 className={classes.titlePreview}>{props.title}</h4>
       </Link>
     );
   }
